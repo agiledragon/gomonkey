392391903626898454822040788d67089d76cd79
agiledragon <zhangxiaolong1980@126.com>
2018-08-18 07:28:36 +0800

    advanced apis define

--- patch.go
+++ patch.go
@@ -1,114 +1,138 @@
 package gomonkey
 
 import (
-    "reflect"
-    "fmt"
-    "unsafe"
-    "syscall"
+	"fmt"
+	"reflect"
+	"syscall"
+	"unsafe"
 )
 
 type Patches struct {
-    originals map[reflect.Value][]byte
+	originals map[reflect.Value][]byte
+}
+
+type Values []interface{}
+type Output struct {
+	Values Values
+	Times  int
 }
 
 func ApplyFunc(target, double interface{}) *Patches {
-    return New().ApplyFunc(target, double)
+	return New().ApplyFunc(target, double)
 }
 
 func ApplyMethod(target reflect.Type, methodName string, double interface{}) *Patches {
-    return New().ApplyMethod(target, methodName, double)
+	return New().ApplyMethod(target, methodName, double)
+}
+
+func ApplyFuncSeq(target interface{}, doubles []Output) *Patches {
+	return New().ApplyFuncSeq(target, doubles)
+}
+
+func ApplyMethodSeq(target reflect.Type, methodName string, doubles []Output) *Patches {
+	return New().ApplyMethodSeq(target, methodName, doubles)
 }
 
 func New() *Patches {
-    return &Patches{make(map[reflect.Value][]byte)}
+	return &Patches{make(map[reflect.Value][]byte)}
 }
 
 func (this *Patches) ApplyFunc(target, double interface{}) *Patches {
-    t := reflect.ValueOf(target)
-    d := reflect.ValueOf(double)
-    this.applyCore(t, d)
-    return this
+	t := reflect.ValueOf(target)
+	d := reflect.ValueOf(double)
+	this.applyCore(t, d)
+	return this
 }
 
 func (this *Patches) ApplyMethod(target reflect.Type, methodName string, double interface{}) *Patches {
-    m, ok := target.MethodByName(methodName);
-    if !ok {
-        panic("retrieve method by name failed")
-    }
-    d := reflect.ValueOf(double)
-    this.applyCore(m.Func, d)
-    return this
+	m, ok := target.MethodByName(methodName)
+	if !ok {
+		panic("retrieve method by name failed")
+	}
+	d := reflect.ValueOf(double)
+	this.applyCore(m.Func, d)
+	return this
+}
+
+func (this *Patches) ApplyFuncSeq(target interface{}, doubles []Output) *Patches {
+	// TODO
+	return this
+}
+
+func (this *Patches) ApplyMethodSeq(target reflect.Type, methodName string, doubles []Output) *Patches {
+	// TODO
+	return this
 }
 
 func (this *Patches) Reset() {
-    for target, bytes := range this.originals {
-        modifyBinary(*(*uintptr)(getPointer(target)), bytes)
-        delete(this.originals, target)
-    }
+	for target, bytes := range this.originals {
+		modifyBinary(*(*uintptr)(getPointer(target)), bytes)
+		delete(this.originals, target)
+	}
 }
 
 func (this *Patches) applyCore(target, double reflect.Value) {
-    this.check(target, double)
-    original := replace(*(*uintptr)(getPointer(target)), uintptr(getPointer(double)))
-    this.originals[target] = original
+	this.check(target, double)
+	original := replace(*(*uintptr)(getPointer(target)), uintptr(getPointer(double)))
+	this.originals[target] = original
 }
 
 func (this *Patches) check(target, double reflect.Value) {
-    if target.Kind() != reflect.Func {
-        panic("target is not a func")
-    }
+	if target.Kind() != reflect.Func {
+		panic("target is not a func")
+	}
 
-    if double.Kind() != reflect.Func {
-        panic("double is not a func")
-    }
+	if double.Kind() != reflect.Func {
+		panic("double is not a func")
+	}
 
-    if target.Type() != double.Type() {
-        panic(fmt.Sprintf("target type(%s) and double type(%s) are different", target.Type(), double.Type()))
-    }
+	if target.Type() != double.Type() {
+		panic(fmt.Sprintf("target type(%s) and double type(%s) are different", target.Type(), double.Type()))
+	}
 
-    if _, ok := this.originals[target]; ok {
-        panic("patch has been existed")
-    }
+	if _, ok := this.originals[target]; ok {
+		panic("patch has been existed")
+	}
 }
 
 func replace(target, double uintptr) []byte {
-    code := buildJmpDirective(double)
-    bytes := entryAddress(target, len(code))
-    original := make([]byte, len(bytes))
-    copy(original, bytes)
-    modifyBinary(target, code)
-    return original
+	code := buildJmpDirective(double)
+	bytes := entryAddress(target, len(code))
+	original := make([]byte, len(bytes))
+	copy(original, bytes)
+	modifyBinary(target, code)
+	return original
 }
 
 type value struct {
-    _ uintptr
-    p unsafe.Pointer
+	_ uintptr
+	p unsafe.Pointer
 }
 
 func getPointer(v reflect.Value) unsafe.Pointer {
-    return (*value)(unsafe.Pointer(&v)).p
+	return (*value)(unsafe.Pointer(&v)).p
 }
 
 func entryAddress(p uintptr, l int) []byte {
-    return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: p, Len: l, Cap: l}))
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: p, Len: l, Cap: l}))
 }
 
 func modifyBinary(target uintptr, bytes []byte) {
-    function := entryAddress(target, len(bytes))
+	function := entryAddress(target, len(bytes))
 
-    page := entryAddress(pageStart(target), syscall.Getpagesize())
-    err := syscall.Mprotect(page, syscall.PROT_READ | syscall.PROT_WRITE | syscall.PROT_EXEC)
-    if err != nil {
-        panic(err)
-    }
-    copy(function, bytes)
+	page := entryAddress(pageStart(target), syscall.Getpagesize())
+	err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC)
+	if err != nil {
+		panic(err)
+	}
+	copy(function, bytes)
 
-    err = syscall.Mprotect(page, syscall.PROT_READ | syscall.PROT_EXEC)
-    if err != nil {
-        panic(err)
-    }
+	err = syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_EXEC)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func pageStart(ptr uintptr) uintptr {
-    return ptr & ^(uintptr(syscall.Getpagesize() - 1))
+	return ptr & ^(uintptr(syscall.Getpagesize() - 1))
 }
