2c85101e476dcd0bdfaa122a0d83fa7c8ac412ac
agiledragon <zhangxiaolong1980@126.com>
2018-12-08 18:17:12 +0800

    support windows

--- modify_binary_windows.go
+++ modify_binary_windows.go
@@ -9,16 +9,16 @@ func modifyBinary(target uintptr, bytes []byte) {
     function := entryAddress(target, len(bytes))
     
     proc := syscall.NewLazyDLL("kernel32.dll").NewProc("VirtualProtect")
-    PROT_READ_WRITE := 0x40
+    const PROT_READ_WRITE = 0x40
     var old uint32
-    result := proc.Call(target, len(bytes), PROT_READ_WRITE, unsafe.Pointer(&old))
+    result, _, _ := proc.Call(target, uintptr(len(bytes)), uintptr(PROT_READ_WRITE), uintptr(unsafe.Pointer(&old)))
     if result == 0 {
         panic(result)
     }
     copy(function, bytes)
     
     var ignore uint32
-    result = proc.Call(target, len(bytes), old, unsafe.Pointer(&ignore))
+    result, _, _ = proc.Call(target, uintptr(len(bytes)), uintptr(old), uintptr(unsafe.Pointer(&ignore)))
     if result == 0 {
         panic(result)
     }
