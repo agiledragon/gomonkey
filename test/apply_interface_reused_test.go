ba746287ab15c2f0d71627cb54e1aa06e532be54
agiledragon <zhangxiaolong1980@126.com>
2019-05-05 21:48:20 +0800

    indents to spaces

--- test/apply_interface_reused_test.go
+++ test/apply_interface_reused_test.go
@@ -2,39 +2,39 @@ package test
 
 import (
     . "github.com/agiledragon/gomonkey"
+    "github.com/agiledragon/gomonkey/test/fake"
     . "github.com/smartystreets/goconvey/convey"
+    "reflect"
     "testing"
-	"reflect"
-	"github.com/agiledragon/gomonkey/test/fake"
 )
 
 func TestApplyInterfaceReused(t *testing.T) {
 
     Convey("TestApplyInterfaceReused", t, func() {
-		e := &fake.Etcd{}
+        e := &fake.Etcd{}
         Convey("TestApplyInterface", func() {
-			patches := ApplyFunc(fake.NewDb, func(_ string) fake.Db {
-				return e
-			})
-			defer patches.Reset()
-			patchForInterface := "support a patch for a interface"
-			patches.ApplyMethod(reflect.TypeOf(e), "Retrieve", 
-				func(_ *fake.Etcd, _ string) (string, error) {
-					return patchForInterface, nil
+            patches := ApplyFunc(fake.NewDb, func(_ string) fake.Db {
+                return e
             })
+            defer patches.Reset()
+            patchForInterface := "support a patch for a interface"
+            patches.ApplyMethod(reflect.TypeOf(e), "Retrieve",
+                func(_ *fake.Etcd, _ string) (string, error) {
+                    return patchForInterface, nil
+                })
             db := fake.NewDb("mysql")
-			output, err := db.Retrieve("")
+            output, err := db.Retrieve("")
             So(err, ShouldEqual, nil)
             So(patchForInterface, ShouldEqual, output)
-		})
-		
-		Convey("TestApplyInterfaceSeq", func() {
-			e := &fake.Etcd{}
-			patches := ApplyFunc(fake.NewDb, func(_ string) fake.Db {
-				return e
-			})
-			defer patches.Reset()
-			info1 := "hello cpp"
+        })
+
+        Convey("TestApplyInterfaceSeq", func() {
+            e := &fake.Etcd{}
+            patches := ApplyFunc(fake.NewDb, func(_ string) fake.Db {
+                return e
+            })
+            defer patches.Reset()
+            info1 := "hello cpp"
             info2 := "hello golang"
             info3 := "hello gomonkey"
             outputs := []OutputCell{
@@ -42,8 +42,8 @@ func TestApplyInterfaceReused(t *testing.T) {
                 {Values: Params{info2, nil}},
                 {Values: Params{info3, nil}},
             }
-			patches.ApplyMethodSeq(reflect.TypeOf(e), "Retrieve", outputs)
-			db := fake.NewDb("mysql")
+            patches.ApplyMethodSeq(reflect.TypeOf(e), "Retrieve", outputs)
+            db := fake.NewDb("mysql")
             output, err := db.Retrieve("")
             So(err, ShouldEqual, nil)
             So(output, ShouldEqual, info1)
@@ -52,7 +52,7 @@ func TestApplyInterfaceReused(t *testing.T) {
             So(output, ShouldEqual, info2)
             output, err = db.Retrieve("")
             So(err, ShouldEqual, nil)
-			So(output, ShouldEqual, info3)
-		})
+            So(output, ShouldEqual, info3)
+        })
     })
 }
