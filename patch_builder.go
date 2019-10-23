08026f2977b6567c4f5a3abff9fda98faed6378a
agiledragon <zhangxiaolong1980@126.com>
2019-10-24 01:01:19 +0800

    format

--- patch_builder.go
+++ patch_builder.go
@@ -1,94 +1,94 @@
 package gomonkey
 
 import (
-	"fmt"
-	"reflect"
+    "fmt"
+    "reflect"
 )
 
 type FuncPara struct {
-	target    interface{}
-	matchers  []Matcher
-	behaviors []ReturnValue
+    target    interface{}
+    matchers  []Matcher
+    behaviors []ReturnValue
 }
 
 type PatchBuilder struct {
-	patches  *Patches
-	funcPara FuncPara
+    patches  *Patches
+    funcPara FuncPara
 }
 
 func NewPatchBuilder(patches *Patches) *PatchBuilder {
-	funcPara := FuncPara{target: nil, matchers: make([]Matcher, 0)}
-	return &PatchBuilder{patches: patches, funcPara: funcPara}
+    funcPara := FuncPara{target: nil, matchers: make([]Matcher, 0)}
+    return &PatchBuilder{patches: patches, funcPara: funcPara}
 }
 
 func (this *PatchBuilder) Func(target interface{}) *PatchBuilder {
-	this.funcPara.target = target
-	return this
+    this.funcPara.target = target
+    return this
 }
 
 func (this *PatchBuilder) Stubs() *PatchBuilder {
-	return this
+    return this
 }
 
 func (this *PatchBuilder) With(matcher ...Matcher) *PatchBuilder {
-	this.funcPara.matchers = append(this.funcPara.matchers, matcher...)
-	return this
+    this.funcPara.matchers = append(this.funcPara.matchers, matcher...)
+    return this
 }
 
 func (this *PatchBuilder) Will(behavior ReturnValue) *PatchBuilder {
-	this.funcPara.behaviors = append(this.funcPara.behaviors, behavior)
-	return this
+    this.funcPara.behaviors = append(this.funcPara.behaviors, behavior)
+    return this
 }
 
 func (this *PatchBuilder) End() {
-	funcType := reflect.TypeOf(this.funcPara.target)
-	t := reflect.ValueOf(this.funcPara.target)
-	d := reflect.MakeFunc(funcType, func(inputs []reflect.Value) []reflect.Value {
-		matchers := this.funcPara.matchers
-		for i, input := range inputs {
-			if !matchers[i].Matches(input.Interface()) {
-				info := fmt.Sprintf("input paras %v is not matched", input.Interface())
-				panic(info)
-			}
-		}
-		return getResultValues(funcType, this.funcPara.behaviors[0].rets...)
-	})
-	this.patches.applyCore(t, d)
+    funcType := reflect.TypeOf(this.funcPara.target)
+    t := reflect.ValueOf(this.funcPara.target)
+    d := reflect.MakeFunc(funcType, func(inputs []reflect.Value) []reflect.Value {
+        matchers := this.funcPara.matchers
+        for i, input := range inputs {
+            if !matchers[i].Matches(input.Interface()) {
+                info := fmt.Sprintf("input paras %v is not matched", input.Interface())
+                panic(info)
+            }
+        }
+        return getResultValues(funcType, this.funcPara.behaviors[0].rets...)
+    })
+    this.patches.applyCore(t, d)
 }
 
 func Any() Matcher {
-	return &AnyMatcher{}
+    return &AnyMatcher{}
 }
 
 func Eq(x interface{}) Matcher {
-	return &EqMatcher{x: x}
+    return &EqMatcher{x: x}
 }
 
 func Return(x ...interface{}) ReturnValue {
-	r := ReturnValue{rets: make([]interface{}, 0)}
-	r.rets = append(r.rets, x...)
-	return r
+    r := ReturnValue{rets: make([]interface{}, 0)}
+    r.rets = append(r.rets, x...)
+    return r
 }
 
 type ReturnValue struct {
-	rets []interface{}
+    rets []interface{}
 }
 
 type Matcher interface {
-	Matches(x interface{}) bool
+    Matches(x interface{}) bool
 }
 
 type AnyMatcher struct {
 }
 
 func (this *AnyMatcher) Matches(x interface{}) bool {
-	return true
+    return true
 }
 
 type EqMatcher struct {
-	x interface{}
+    x interface{}
 }
 
 func (this *EqMatcher) Matches(x interface{}) bool {
-	return reflect.DeepEqual(this.x, x)
+    return reflect.DeepEqual(this.x, x)
 }
