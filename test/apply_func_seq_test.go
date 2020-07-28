b1518b87263424986d40340625bebae1931a1503
agiledragon <zhangxiaolong1980@126.com>
2020-07-28 22:32:10 +0800

    add GC call to seq test

--- test/apply_func_seq_test.go
+++ test/apply_func_seq_test.go
@@ -1,72 +1,76 @@
 package test
 
 import (
-    . "github.com/agiledragon/gomonkey"
-    . "github.com/smartystreets/goconvey/convey"
-    "testing"
-    "github.com/agiledragon/gomonkey/test/fake"
+	"runtime"
+	"testing"
+
+	. "github.com/agiledragon/gomonkey"
+	"github.com/agiledragon/gomonkey/test/fake"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestApplyFuncSeq(t *testing.T) {
-    Convey("TestApplyFuncSeq", t, func() {
+	Convey("TestApplyFuncSeq", t, func() {
 
-        Convey("default times is 1", func() {
-            info1 := "hello cpp"
-            info2 := "hello golang"
-            info3 := "hello gomonkey"
-            outputs := []OutputCell{
-                {Values: Params{info1, nil}},
-                {Values: Params{info2, nil}},
-                {Values: Params{info3, nil}},
-            }
-            patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
-            defer patches.Reset()
-            output, err := fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info1)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info2)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info3)
-        })
+		Convey("default times is 1", func() {
+			info1 := "hello cpp"
+			info2 := "hello golang"
+			info3 := "hello gomonkey"
+			outputs := []OutputCell{
+				{Values: Params{info1, nil}},
+				{Values: Params{info2, nil}},
+				{Values: Params{info3, nil}},
+			}
+			patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
+			defer patches.Reset()
 
-        Convey("retry succ util the third times", func() {
-            info1 := "hello cpp"
-            outputs := []OutputCell{
-                {Values: Params{"", fake.ErrActual}, Times: 2},
-                {Values: Params{info1, nil}},
-            }
-            patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
-            defer patches.Reset()
-            output, err := fake.ReadLeaf("")
-            So(err, ShouldEqual, fake.ErrActual)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, fake.ErrActual)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info1)
-        })
+			runtime.GC()
 
-        Convey("batch operations failed on the third time", func() {
-            info1 := "hello gomonkey"
-            outputs := []OutputCell{
-                {Values: Params{info1, nil}, Times: 2},
-                {Values: Params{"", fake.ErrActual}},
-            }
-            patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
-            defer patches.Reset()
-            output, err := fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info1)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, nil)
-            So(output, ShouldEqual, info1)
-            output, err = fake.ReadLeaf("")
-            So(err, ShouldEqual, fake.ErrActual)
-        })
+			output, err := fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info1)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info2)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info3)
+		})
 
-    })
+		Convey("retry succ util the third times", func() {
+			info1 := "hello cpp"
+			outputs := []OutputCell{
+				{Values: Params{"", fake.ErrActual}, Times: 2},
+				{Values: Params{info1, nil}},
+			}
+			patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
+			defer patches.Reset()
+			output, err := fake.ReadLeaf("")
+			So(err, ShouldEqual, fake.ErrActual)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, fake.ErrActual)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info1)
+		})
+
+		Convey("batch operations failed on the third time", func() {
+			info1 := "hello gomonkey"
+			outputs := []OutputCell{
+				{Values: Params{info1, nil}, Times: 2},
+				{Values: Params{"", fake.ErrActual}},
+			}
+			patches := ApplyFuncSeq(fake.ReadLeaf, outputs)
+			defer patches.Reset()
+			output, err := fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info1)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, nil)
+			So(output, ShouldEqual, info1)
+			output, err = fake.ReadLeaf("")
+			So(err, ShouldEqual, fake.ErrActual)
+		})
+
+	})
 }
-
