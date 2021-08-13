f94b859664007684263c77ecdefd555c4c31e5db
wuheng <w._heng@163.com>
2021-08-13 19:20:41 +0800

    fix arm64 test failed in some TestFunctions
    All tests passed now, and arm64 is fully supported.

--- jmp_arm64.go
+++ jmp_arm64.go
@@ -3,7 +3,7 @@ package gomonkey
 import "unsafe"
 
 func movX(opc, shift int, val uintptr) []byte {
-	var m uint32 = 27          // rd
+	var m uint32 = 26          // rd
 	m |= uint32(val) << 5      // imm16
 	m |= uint32(shift&3) << 21 // hw
 	m |= 0b100101 << 23        // const
@@ -16,11 +16,13 @@ func movX(opc, shift int, val uintptr) []byte {
 	return res
 }
 
-func buildJmpDirective(targetAddr uintptr) []byte {
-	targetFuncAddr := *(*uintptr)(unsafe.Pointer(targetAddr)) //func address
-	res := make([]byte, 0, 12)
-	res = append(res, movX(0b10, 0, targetFuncAddr&0xffff)...)     //movz x27, addr[16:]
-	res = append(res, movX(0b11, 1, targetFuncAddr>>16&0xffff)...) //movk x27, addr[32:16]
-	res = append(res, []byte{0x60, 0x03, 0x1f, 0xd6}...)           //br  x27
+func buildJmpDirective(targetFuncAddr uintptr) []byte {
+	res := make([]byte, 0, 24)
+	res = append(res, movX(0b10, 0, targetFuncAddr&0xffff)...)     //movz x26, addr[16:]
+	res = append(res, movX(0b11, 1, targetFuncAddr>>16&0xffff)...) //movk x26, addr[32:16]
+	res = append(res, movX(0b11, 2, targetFuncAddr>>32&0xffff)...) //movk x26, addr[48:32]
+	res = append(res, movX(0b11, 3, targetFuncAddr>>48&0xffff)...) //movk x26, addr[64:48]
+	res = append(res, []byte{0x4a, 0x03, 0x40, 0xf9}...)           //ldr  x10, [x26]
+	res = append(res, []byte{0x40, 0x01, 0x1f, 0xd6}...)           //br   x10
 	return res
 }
